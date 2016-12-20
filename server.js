52c3e76b14ed36784f9d5fc5f2bb5b9de48db5e5
matthew <matthewjf@gmail.com>
2016-12-19 21:16:18 -0800

    added server and routes

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+// APP
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// LIRC NODE
+var lircNode = require('lirc_node');
+lircNode.init();
+
+// ROUTES
+var router = express.Router();
+var remotes = require('./remotes');
+
+router.route('/').get(function(req, res) {
+  res.status(200).json(remotes.all());
+});
+
+router.route('/list').get(function(req, res) {
+  res.status(200).json(remotes.list());
+});
+
+router.route('/list/:remote').get(function(req, res) {
+  res.status(200).json(remotes.get(req.params.remote));
+});
+
+router.route('/remotes/:remote/:command').get(function(req, res) {
+
+});
+
+app.use('/api', router);
+
+// START
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
