2797b9166952a3e8dd2a522b5f39df4b05ad8b8e
matthew <matthewjf@gmail.com>
2016-12-21 07:55:12 -0800

    added irrercord cmd

--- ir_receive.js
+++ ir_receive.js
@@ -1,17 +1,27 @@
 var spawn = require('child_process').spawn;
+var data = {};
 var res;
 
 var startRecord = function(response) {
   res = response;
-  // var cmd = "irrecord -d /dev/lirc0 ~/lircd.conf\n";
-  var cmd = "\n";
+  var cmd = "irrecord -d /dev/lirc0 ~/lircd.conf\n";
   var child = spawn('bash');
   child.stdin.setEncoding('utf-8');
-  child.stdout.pipe(process.stdout);
-  child.stdin.write("pwd\n");
+  
+  child.stdout.on('data', function(output) {
+    data = output;
+  });
+
+  child.stdout.on('close', function(code) {
+    console.log('exited with code: ', code);
+  });
+
+  child.stdin.write(cmd);
+  child.stdin.write('\n');
+
   child.stdin.end();
 
-  res.status(200).json('ok');
+  res.status(200).json(data);
 };
 
 module.exports = {
