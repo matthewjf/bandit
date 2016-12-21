071cfe9db4e21855630756d7d0217621881933ed
matthew <matthewjf@gmail.com>
2016-12-20 23:56:44 -0800

    test spawn methods

--- ir_receive.js
+++ ir_receive.js
@@ -3,18 +3,12 @@ var res;
 
 var startRecord = function(response) {
   res = response;
-  var cmd = "irrecord -d /dev/lirc0 ~/lircd.conf\n";
-  var child = spawn(cmd);
-
-  child.stdout.on('data', function(data) {
-    if (res) res.status(200).json(data);
-  });
-
-  child.on('exit', function(code) {
-    if (res) res.status(200).json('exit');
-  });
-
-  // child.stdin.write(cmd);
+  // var cmd = "irrecord -d /dev/lirc0 ~/lircd.conf\n";
+  var cmd = "\n";
+  var child = spawn('bash');
+  child.stdout.on('data', function(data) {console.log(data);});
+  child.stdin.write("pwd\n");
+  child.stdin.end();
 };
 
 module.exports = {
